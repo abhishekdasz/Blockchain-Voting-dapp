93fb5b56c2b70b95142d1445e7b457a803fb2475
Abhishek <abhishekdaszy@gmail.com>
2023-11-21 01:14:57 +0530

    added a welcome page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,25 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+  const router = useRouter();
+
+  const handleVoterLogin = () => {
+    router.push('/voter')
+  }
+  const handleAdminLogin = () => {
+    router.push('/admin')
+  }
   return (
     <div>
       Hello World
+      <div className="voter">
+        <button onClick={handleVoterLogin}> Voter Login/Sign up </button>
+      </div>
+      <div className="admin">
+        <button onClick={handleAdminLogin}> Admin Login </button>
+      </div>
     </div>
   )
 }
