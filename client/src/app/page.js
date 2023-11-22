50f4702312e5493737db455d13d1f6de8b276389
Abhishek <abhishekdaszy@gmail.com>
2023-11-22 18:34:55 +0530

    added css for home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,27 +1,39 @@
-'use client'
-import React from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
 
 const page = () => {
   const router = useRouter();
 
   const handleVoterLogin = () => {
-    router.push('/voter')
-  }
+    router.push("/voter");
+  };
   const handleAdminLogin = () => {
-    router.push('/admin')
-  }
+    router.push("/admin");
+  };
   return (
-    <div>
-      Hello World
-      <div className="voter">
-        <button onClick={handleVoterLogin}> Voter Login/Sign up </button>
-      </div>
-      <div className="admin">
-        <button onClick={handleAdminLogin}> Admin Login </button>
+    <div className={styles.homeSec}>
+      <div className={styles.homeDiv}>
+        <div className={styles.leftSec}>
+          <div className={styles.header}>
+            <h1> Welcome to Our <br /> Voting Platform </h1>
+            <h3> Make your voice heard! </h3>
+            <h3> We ensure a secure and transparent voting process </h3>
+
+            <div className={styles.buttons}>
+              <div className={styles.btn} onClick={handleVoterLogin}> Voter Login/Sign up </div>
+              <div className={styles.btn} onClick={handleAdminLogin}> Admin Login </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.rightSec}>
+          <img src="home.png" alt="home" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
