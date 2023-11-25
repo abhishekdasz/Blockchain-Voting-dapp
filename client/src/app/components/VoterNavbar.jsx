01c1127cdf6919ef71f39ef02f4753de142d2740
Abhishek <abhishekdaszy@gmail.com>
2023-11-25 18:34:12 +0530

    delete some unused files

--- client/src/app/components/VoterNavbar.jsx
+++ client/src/app/components/VoterNavbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import "./AdminNavbar.css";
 
 const VoterNavbar = () => {
   const [active, setActive] = useState();
