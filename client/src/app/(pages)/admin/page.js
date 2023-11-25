c70ca72fdd73ffb02621898b308c53ed9c31c2ea
Abhishek <abhishekdaszy@gmail.com>
2023-11-25 18:46:07 +0530

    update votingStatus

--- client/src/app/(pages)/admin/page.js
+++ client/src/app/(pages)/admin/page.js
@@ -1,103 +1,108 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import abi from '../../contract/Voting.json';
-import { useRouter } from 'next/navigation';
-import CON_ADDRESS from '@/app/constants';
-
+import abi from "../../contract/Voting.json";
+import { useRouter } from "next/navigation";
+import CON_ADDRESS from "@/app/constants";
 
 const Page = () => {
-    const router = useRouter();
-    const [provider, setProvider] = useState(null);
+  const router = useRouter();
+  const [provider, setProvider] = useState(null);
 
-    const [account, setAccount] = useState(null);
-    const [isConnected, setIsConnected] = useState(false);
-    const [votingStatus, setVotingStatus] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    const contractAddress = CON_ADDRESS;
-    const contractAbi = abi.abi;
+  const contractAddress = CON_ADDRESS;
+  const contractAbi = abi.abi;
 
-    // Connect to Metamask and check if the account is an admin
-    const connectContract = async () => {
-        if (window.ethereum) {
-            try {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
-                setProvider(provider);
-                await provider.send("eth_requestAccounts", []);
-                const signer = provider.getSigner();
-                const address = await signer.getAddress();
-                setAccount(address);
-                console.log("Metamask connected" + address);
-                setIsConnected(true);
-
-                // Check if the connected account is an admin
-                const contractInstance = new ethers.Contract (
-                    contractAddress, contractAbi, signer
-                );
-                const adminStatus = await contractInstance.isAdmin();
-                setIsAdmin(adminStatus);
-
-                if (!adminStatus) {
-                    alert("You are not logged in as an admin. Please log in with your admin account");
-                    router.push('/')
-                }
-                else 
-                {
-                    // Redirect to admin/dashboard after successful login
-                    router.push('/admin/adminDash');
-                }
-
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    };
-
-    useEffect(() => {
-        connectContract();
-        getCurrentStatus();
-        if (window.ethereum) {
-            window.ethereum.on('accountsChanged', handleAccountsChanged);
-        }
-
-        return () => {
-            if (window.ethereum) {
-                window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
-            }
-        }
-    }, [account]);
-
-    function handleAccountsChanged(accounts) {
-        if (accounts.length > 0 && account !== accounts[0]) {
-            setAccount(accounts[0]);
-        } else {
-            setIsConnected(false);
-            setAccount(null);
-            setIsAdmin(false); // Reset isAdmin state when disconnected
-        }
-    }
-
-    const getCurrentStatus = async () => {
+  // Connect to Metamask and check if the account is an admin
+  const connectContract = async () => {
+    if (window.ethereum) {
+      try {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
-        const contractInstance = new ethers.Contract (
-            contractAddress, contractAbi, signer
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask connected" + address);
+        setIsConnected(true);
+
+        // Check if the connected account is an admin
+        const contractInstance = new ethers.Contract(
+          contractAddress,
+          contractAbi,
+          signer
         );
-        const status = await contractInstance.getVotingStatus();
-        console.log(status);
-        setVotingStatus(status);
+        const adminStatus = await contractInstance.isAdmin();
+        setIsAdmin(adminStatus);
+
+        if (!adminStatus) {
+          alert(
+            "You are not logged in as an admin. Please log in with your admin account"
+          );
+          router.push("/");
+        } else {
+          // Redirect to admin/dashboard after successful login
+          router.push("/admin/adminDash");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    connectContract();
+    getCurrentStatus();
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
     }
 
-    const handleAdminLogout = () => {
-      router.push('/')
-    }
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      }
+    };
+  }, [account]);
 
-    return (
-        <div>
-            Admin
-            {/* <div className="login-admin">
+  function handleAccountsChanged(accounts) {
+    if (accounts.length > 0 && account !== accounts[0]) {
+      setAccount(accounts[0]);
+    } else {
+      setIsConnected(false);
+      setAccount(null);
+      setIsAdmin(false); // Reset isAdmin state when disconnected
+    }
+  }
+
+  const getCurrentStatus = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    const status = await contractInstance.getVotingStatus();
+    console.log(status);
+    setVotingStatus(status);
+  };
+
+  const handleAdminLogout = () => {
+    router.push("/");
+  };
+
+  return (
+    <div>
+      Admin
+      {/* <div className="login-admin">
             {isConnected && isAdmin ? ( // Only render AddCandidate if isConnected and isAdmin
                 <AdminDashboard account={account} provider={provider} />
             ) : (
@@ -107,8 +112,8 @@ const Page = () => {
             <div className="logout">
               <button onClick={handleAdminLogout}> Logout </button>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Page;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -17,13 +17,31 @@ const page = () => {
       <div className={styles.homeDiv}>
         <div className={styles.leftSec}>
           <div className={styles.header}>
-            <h1 style={{color:'#fff'}}>Welcome to Our <br /> Secure & Transparent <br /> Voting Platform</h1>
-            <h3 style={{color:'#fff'}}>Make your voice heard in a digital democracy designed for trust and integrity.</h3>
-            <p>At our voting platform, we prioritize the security and transparency of every vote. Our cutting-edge technology ensures that your vote is confidential, tamper-proof, and accurately counted. Join us in shaping the future by participating in a voting process that empowers individuals and strengthens the foundations of democracy.</p>
+            <h1 style={{ color: "#fff" }}>
+              Welcome to Our <br /> Secure & Transparent <br /> Voting Platform
+            </h1>
+            <h3 style={{ color: "#fff" }}>
+              Make your voice heard in a digital democracy designed for trust
+              and integrity.
+            </h3>
+            <p>
+              At our voting platform, we prioritize the security and
+              transparency of every vote. Our cutting-edge technology ensures
+              that your vote is confidential, tamper-proof, and accurately
+              counted. Join us in shaping the future by participating in a
+              voting process that empowers individuals and strengthens the
+              foundations of democracy.
+            </p>
 
             <div className={styles.buttons}>
-              <div className={styles.btn} onClick={handleAdminLogin}> Admin Login </div>
-              <div className={styles.btn} onClick={handleVoterLogin}> Voter Login/Sign up </div>
+              <div className={styles.btn} onClick={handleAdminLogin}>
+                {" "}
+                Admin Login{" "}
+              </div>
+              <div className={styles.btn} onClick={handleVoterLogin}>
+                {" "}
+                Voter Login/Sign up{" "}
+              </div>
             </div>
           </div>
         </div>
